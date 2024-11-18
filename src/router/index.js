0d5aaad51b37e0975d22d03b87b7f68e8450209c
emmaL44 <141998897+emmaL44@users.noreply.github.com>
2024-11-18 21:25:02 +0400

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Cube from '../components/cube/Cube.vue'
+import XrCube from '../components/cube/XrCube.vue'
+import XrModel from '../components/hit-test/XrModel.vue'
+import XrModelOverlay from '../components/dom-overlay/XrModelOverlay.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Cube
+    },
+    {
+      path: '/cube',
+      name: 'cube',
+      component: Cube
+    },
+
+    {
+      path: '/xr-cube',
+      name: 'xr-cube',
+      component: XrCube
+    },
+    {
+      path: '/xr-model',
+      name: 'xr-model',
+      component: XrModel
+    },
+    {
+      path: '/xr-model-overlay',
+      name: 'xr-model-overlay',
+      component: XrModelOverlay
+    },
+
+  ]
+})
+
+export default router
